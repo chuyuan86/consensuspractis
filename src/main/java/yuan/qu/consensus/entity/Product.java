6477e6f4f8320f13b340539c563d621c7b224650
chuyuan <chrisqu@163.com>
2017-12-21 11:14:03 +0800

    初始化项目

---
+++ src/main/java/yuan/qu/consensus/entity/Product.java
@@ -0,0 +1,15 @@
+package yuan.qu.consensus.entity;
+
+public class Product {
+
+    private Long id;
+
+    private String name;
+
+    private Long sold;
+
+    private Long quantity;
+
+
+
+}
---
+++ src/main/java/yuan/qu/consensus/local/LocalCache.java
@@ -0,0 +1,11 @@
+package yuan.qu.consensus.local;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LocalCache {
+
+    public static Map<String,Long> productCount = new HashMap<String, Long>();
+
+
+}
