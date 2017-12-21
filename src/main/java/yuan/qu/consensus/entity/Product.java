1d4d76a17b8d44a32bfd4ab7a90e371fbb3b256f
chuyuan <chrisqu@163.com>
2017-12-21 11:35:44 +0800

    setter getter

--- src/main/java/yuan/qu/consensus/entity/Product.java
+++ src/main/java/yuan/qu/consensus/entity/Product.java
@@ -10,6 +10,35 @@ public class Product {
 
     private Long quantity;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getSold() {
+        return sold;
+    }
+
+    public void setSold(Long sold) {
+        this.sold = sold;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
 }
