7907e83f08fa204921c28516750ac526be70e99d
chuyuan <chrisqu@163.com>
2017-12-27 14:58:59 +0800

    加入springmvc

--- src/main/java/yuan/qu/consensus/api/ProductController.java
+++ src/main/java/yuan/qu/consensus/api/ProductController.java
@@ -2,10 +2,15 @@ package yuan.qu.consensus.api;
 
 import yuan.qu.consensus.service.ProductService;
 
+/**
+ * 抢购用
+ * */
 public class ProductController {
 
     ProductService productService;
 
 
+    public void order(){
 
+    }
 }
--- src/main/java/yuan/qu/consensus/service/impl/ProductServiceImpl.java
+++ src/main/java/yuan/qu/consensus/service/impl/ProductServiceImpl.java
@@ -5,12 +5,34 @@ import yuan.qu.consensus.service.ProductService;
 
 public class ProductServiceImpl implements ProductService{
 
+    /**
+     * 1:redis
+     * 2:memory
+     * 3:db
+     * */
+    private static int strategy;
 
     public Long getProductQuantity(Long id) {
+        switch(this.strategy){
+            case 1:
+                System.out.print("redis");
+            case 2:
+                System.out.print("memory");
+            case 3:
+                System.out.print("db");
+        }
         return null;
     }
 
     public boolean addOrder(Order o) {
+        switch(this.strategy){
+            case 1:
+                System.out.print("redis");
+            case 2:
+                System.out.print("memory");
+            case 3:
+                System.out.print("db");
+        }
         return false;
     }
 }
